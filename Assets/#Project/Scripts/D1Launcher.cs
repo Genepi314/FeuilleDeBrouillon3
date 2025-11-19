a82e32f6ddc644852d06326c17ec5d42660f5938
genepi314 <gen_ernould@hotmail.com>
2025-11-19 15:46:02 +0100

    First dialogue test complete

--- Assets/#Project/Scripts/D1Launcher.cs
+++ Assets/#Project/Scripts/D1Launcher.cs
@@ -8,17 +8,48 @@ public class D1Launcher : MonoBehaviour
     [SerializeField] GameObject dialogueDisplay;
     [SerializeField] TextMeshProUGUI characterNameArea;
     [SerializeField] TextMeshProUGUI dialogueLineArea;
+    private int dialogueStartIndex = 0;
+    private int nextLineIndex;
+    private bool dialogueStarted = false;
 
-    void Start()
-    {
-        dialogueDisplay.SetActive(true);
-    }
+    // void Start()
+    // {
+
+    // }
 
     public void ButtonInteractPressed()
     {
-        Debug.Log("ButtonInteractPressed");
+        if(dialogueStarted == false)
+        {
+            StartDialogue();
+            dialogueStarted = true;
+        }
+        else if (npcDial1.dialogue.Count > nextLineIndex)
+        {
+            characterNameArea.text = npcDial1.dialogue[nextLineIndex].characterName;
+            dialogueLineArea.text = npcDial1.dialogue[nextLineIndex].sentence;
+            
+        }
+        nextLineIndex ++;
+        if (npcDial1.dialogue.Count < nextLineIndex)
+        {
+            EndDialogue();
+            dialogueStarted = false;
+            nextLineIndex = dialogueStartIndex;
+        }
+    }
+
+    private void StartDialogue()
+    {
+        dialogueDisplay.SetActive(true);
+        // Debug.Log("ButtonInteractPressed");
         characterNameArea.text = npcDial1.dialogue[0].characterName;
         dialogueLineArea.text = npcDial1.dialogue[0].sentence;
+    }
+
+    private void EndDialogue()
+    {
+        dialogueDisplay.SetActive(false);
     }
 
 }
