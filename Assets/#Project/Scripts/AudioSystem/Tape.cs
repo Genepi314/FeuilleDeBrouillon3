18578b90126b0db24928c88965db1152c7209f0a
genepi314 <gen_ernould@hotmail.com>
2025-12-03 18:15:21 +0100

    added new C# scripts Tape and TrackList for future final Recorder.

---
+++ Assets/#Project/Scripts/AudioSystem/Tape.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Tape
+{
+    public AudioSource audioSource;
+    public Vector3 distanceToPlayer;
+    public float sampleStartTime;
+    public float unityStartTime;
+    public float unityEndTime;
+}
---
+++ Assets/#Project/Scripts/AudioSystem/TrackList.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public static class TrackList
+{
+    public static List<Tape> tapes;
+}
